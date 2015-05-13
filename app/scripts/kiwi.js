16b6a2da5418470ced6540be24c31c68f9f00634
Hayden <hayden.king@gmail.com>
2015-05-12 23:12:47 -0500

    remove extra javascript causing chat to reload everytime it is resized; add jquery vendor.

--- app/scripts/kiwi.js
+++ app/scripts/kiwi.js
@@ -1,46 +1,7 @@
-var ident = Math.floor((Math.random() * 1000) + 100);
-
-function startChat()
+$(function()
 {
-  document.getElementById('kiwi').src = "https://kiwiirc.com/client?settings=e9286b604ad025752393eb5d37329b68&nick=BootHelp_" + ident;
-}
+    var ident = Math.floor((Math.random() * 1000) + 100);
 
-function autoResize()
-{
+    $('#kiwi').attr('src', "https://kiwiirc.com/client?settings=e9286b604ad025752393eb5d37329b68&nick=BootHelp_" + ident);
 
-  newWidth = document.body.clientWidth;
-  newHeight = document.body.clientHeight;
-
-  if(newWidth == null){
-
-    newWidth = window.innerWidth;
-
-  }
-
-  if(newHeight == null){
-
-    newHeight = window.innerHeight;
-
-  }
-
-  if(newWidth <= 50){
-
-    newWidth = 50;
-
-  }
-
-  if(newHeight <= 50){
-
-    newHeight = 50;
-
-  }
-
-  document.getElementById('kiwi').style.width = newWidth;
-  document.getElementById('kiwi').style.height = newHeight - 12;
-
-  startChat();
-
-}
-
-window.onload = autoResize;
-window.onresize = autoResize;
+});
